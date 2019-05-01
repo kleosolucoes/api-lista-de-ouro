034059e7ca6f9272bcac8776d78d38efb14be0b3
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-05-01 17:48:00 -0300

    validando dados ao sincronizar

--- src/controllers/no.controller.js
+++ src/controllers/no.controller.js
@@ -14,41 +14,44 @@ exports.sincronizar = async (req, res, next) => {
 		objetoDeRetorno.resultado = {}
 		if(req.body.email && req.body.senha){
 
-		const noSelecionado = await No.findOne({email: req.body.email})
-		if(noSelecionado === null){
-			objetoDeRetorno.menssagem = 'Não registrado'
-			return res.send(objetoDeRetorno)
-		}
-		const senhaTexto = req.body.senha + ''
-		const senhasIguais = bcrypt.compareSync(senhaTexto, noSelecionado.senha)
-		if(senhasIguais){
-			/* senha correta */
-			objetoDeRetorno.ok = true
-			objetoDeRetorno.menssagem = 'Logado!!!'
+			const noSelecionado = await No.findOne({email: req.body.email})
+			if(noSelecionado === null){
+				objetoDeRetorno.menssagem = 'Não registrado'
+				return res.send(objetoDeRetorno)
+			}
+			const senhaTexto = req.body.senha + ''
+			const senhasIguais = bcrypt.compareSync(senhaTexto, noSelecionado.senha)
+			if(senhasIguais){
+				/* senha correta */
+				objetoDeRetorno.ok = true
+				objetoDeRetorno.menssagem = 'Logado!!!'
 
-			/* buscando prospectos para baixar */
-			const prospectosDaWeb = await Prospecto.find({no_id: noSelecionado._id, sincronizado: false})
-			objetoDeRetorno.resultado.prospectos = prospectosDaWeb
-			prospectosDaWeb.forEach(prospecto => {
-				prospecto.sincronizado = true
-				prospecto.save((err, res) => console.log('prospecto salvo'))
-			})
-			/* buscando acoes do patricionador */
-			const acoesDoPatricionador = await Acao.find({no_id: noSelecionado.pai_id})
-			objetoDeRetorno.resultado.acoes = acoesDoPatricionador
-			/* Subindo historico */
-			if(req.body.historicos){
-				req.body.historicos.forEach(historico => {
-					const historicoNovo = new Historico({
-						sincronizado: false,
-						data_criacao: historico.data_criacao,
-						hora_criacao: historico.hora_criacao,
-						dados: historico.dados,
-						no_id: noSelecionado._id,
-					})
-					historicoNovo.save((err, res) => console.log('historico salvo'))
+				/* buscando prospectos para baixar */
+				const prospectosDaWeb = await Prospecto.find({no_id: noSelecionado._id, sincronizado: false})
+				objetoDeRetorno.resultado.prospectos = prospectosDaWeb
+				prospectosDaWeb.forEach(prospecto => {
+					prospecto.sincronizado = true
+					prospecto.save((err, res) => console.log('prospecto salvo'))
 				})
-				objetoDeRetorno.resultado.historico = 'Historico Subiu'
+				/* buscando acoes do patricionador */
+				const acoesDoPatricionador = await Acao.find({no_id: noSelecionado.pai_id})
+				objetoDeRetorno.resultado.acoes = acoesDoPatricionador
+				/* Subindo historico */
+				if(req.body.historicos){
+					req.body.historicos.forEach(historico => {
+						const historicoNovo = new Historico({
+							sincronizado: false,
+							data_criacao: historico.data_criacao,
+							hora_criacao: historico.hora_criacao,
+							dados: historico.dados,
+							no_id: noSelecionado._id,
+						})
+						historicoNovo.save((err, res) => console.log('historico salvo'))
+					})
+					objetoDeRetorno.resultado.historico = 'Historico Subiu'
+				}
+			}else{
+				objetoDeRetorno.menssagem = 'Senha não iguais'
 			}
 		}else{
 			objetoDeRetorno.menssagem = 'Senha não iguais'
@@ -58,8 +61,5 @@ exports.sincronizar = async (req, res, next) => {
 		objetoDeRetorno.menssagem = error
 		return res.send(objetoDeRetorno)
 	}
-	}else{
-		objetoDeRetorno.menssagem = 'Dados invalidos'
-		return res.send(objetoDeRetorno)
-	}
+
 }
