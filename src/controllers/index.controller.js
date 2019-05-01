eaaeb6d5ed1d6bfb463905652bd3afaf536812ea
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-05-01 17:41:38 -0300

    index route ajustada

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,5 +2,8 @@ import No from '../models/no.model'
 import { objetoDeRetorno, gerarToken, enviarEmail } from '../constants'
 
 exports.index = (req, res, next) => {
-	res.send('api lista de ouro')
+	objetoDeRetorno.ok = true 
+	objetoDeRetorno.menssagem = 'API Lista de Ouro'
+	objetoDeRetorno.resultado = {}
+	return res.send(objetoDeRetorno)
 }
